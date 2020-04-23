d94a23961dca85b8d5eba2970c2cab9ba4a7f29c
Marco Nuñez <marco.soulg@gmail.com>
2020-04-22 21:07:45 -0500

    install nunit

---
+++ Tests/DummyTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace TweetishApp.Tests
+{
+    [TestFixture]
+    public class DummyTest
+    {
+        [Test]
+        public void testDummy()
+        {
+            Assert.True(true);
+        }
+    }
+}
