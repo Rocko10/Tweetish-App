e49fc28589aaa7727de4bcca91b09dc2f38ad4c2
Marco Nuñez <marco.soulg@gmail.com>
2020-05-01 14:43:36 -0500

    add react

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace TweetishApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
