ac981e73f955738311f8363a029d76d80d470dbd
Marco Nuñez <marco.soulg@gmail.com>
2020-05-17 15:14:50 -0500

    add user tweet reaction service

---
+++ Core/Services/UserTweetReactionService.cs
@@ -0,0 +1,21 @@
+using TweetishApp.Core.Interfaces;
+using TweetishApp.Core.Entities;
+using System.Threading.Tasks;
+
+namespace TweetishApp.Core.Services
+{
+    public class UserTweetReactionService
+    {
+        private IUserTweetReactionRepository _repository;
+
+        public UserTweetReactionService(IUserTweetReactionRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<UserTweetReaction> Toggle(UserTweetReaction userTweetReaction)
+        {
+            return await _repository.Toggle(userTweetReaction);
+        }
+    }
+}
